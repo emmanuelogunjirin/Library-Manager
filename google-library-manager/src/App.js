a6965baec865a62a8ae3365bbbe2bf4f2a61ea7a
Emmanuel Ogunjirin <eao5xc@virginia.edu>
2020-05-12 19:04:19 -0400

    Successfully Deployed to Firebase

--- google-library-manager/src/App.js
+++ google-library-manager/src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> file and save to reload the.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React, { useState, useEffect } from 'react';
+import firebase from './Firebase';
+const firebasedb = firebase.firestore();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> file and save to reload the.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [books, setBooks] = useState(null);
+
+  useEffect(() => {
+    fetch("/api/books?title=the+outsiders")
+    .then(res => res.json())
+    .then(data => setBooks(data));
+    addBook();
+  }, []);
+
+  function DisplayBooks() {
+    console.log(books);
+    return(
+      <ul>
+        {books.map(book => (
+          <li>{book.title}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  function addBook() {
+    console.log("Starting");
+    console.log(
+    
+    firebasedb.collection("cities").doc("LA").set({
+      name: "Los Angeles",
+      state: "CA",
+      country: "USA"
+  }).then(function() {
+    console.log("Document successfully written!");
+}));
+  }
+
+  if(!books)
+  {
+    return(
+      <div>
+        <h1>Failed to find books</h1>
+      </div>
+    )
+  }
+
+  return <DisplayBooks></DisplayBooks>;
 }
+
+// if(books)
+// {
+
+// }
 
 export default App;
--- google-library-manager/src/Firebase.js
+++ google-library-manager/src/Firebase.js
@@ -2,12 +2,14 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const config = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTHORIZATION_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTHORIZATION_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_IDENTIFICATION
 };
 
-firebase.initializeApp(config);
+var projectId = process.env.PROJECT_IDENTIFICATION;
+console.log(projectId);
+firebase.initializeApp(firebaseConfig);
 export default firebase;
