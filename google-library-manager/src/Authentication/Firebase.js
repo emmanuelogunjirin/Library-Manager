3a7d19bf52eafac0b21d9cbef9acef599d0f4d36
Emmanuel Ogunjirin <eao5xc@virginia.edu>
2020-05-13 20:15:55 -0400

    Added all values to firebase authentication service

--- google-library-manager/src/Authentication/Firebase.js
+++ google-library-manager/src/Authentication/Firebase.js
@@ -6,7 +6,11 @@ const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTHORIZATION_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_IDENTIFICATION
+  projectId: process.env.REACT_APP_PROJECT_IDENTIFICATION,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSender: process.env.REACT_APP_MESSAGING_SENDER,
+  applicationIdentification: process.env.REACT_APP_APPLICATION_IDENTIFICATION,
+  measurementIdentification: process.env.REACT_APP_MEASUREMENT_IDENTIFICATION
 };
 
 firebase.initializeApp(firebaseConfig);
