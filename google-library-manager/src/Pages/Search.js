3fbb474d77e582ed3d94fa455a26a163cf75f87b
Emmanuel Ogunjirin <eao5xc@virginia.edu>
2020-05-18 02:29:42 -0400

    Got response from search textbox

--- google-library-manager/src/Pages/Search.js
+++ google-library-manager/src/Pages/Search.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import useStyles from "../Containers/User/UserStyles";
 import MainTheme from "../Containers/User/MainTheme";
 import TextField from "@material-ui/core/TextField";
@@ -6,6 +6,54 @@ import Container from "@material-ui/core/Container";
 
 function HomeSetup() {
   const classes = useStyles();
+  const [books, setBooks] = useState(null);
+  const [title, setTitle] = useState(null);
+  const link = `/api/books?title=${title}`;
+
+  const handleClick = (event) => {
+    if (event) {
+      setTitle(event);
+    }
+  };
+
+  useEffect(() => {
+    fetch(link)
+      .then((res) => res.json())
+      .then((data) => setBooks(data));
+    // addBook();
+  });
+
+  function DisplayBooks() {
+    console.log(books);
+    if (books) {
+      return (
+        <ul>
+          {books.map((book) => (
+            <li>{book.image}</li>
+          ))}
+        </ul>
+      );
+    } else {
+      return <li>Cannot Find Book</li>;
+    }
+  }
+
+  //   function addBook() {
+  //     console.log("Starting");
+  //     console.log(
+  //       firebasedb
+  //         .collection("cities")
+  //         .doc("LA")
+  //         .set({
+  //           name: "Los Angeles",
+  //           state: "CA",
+  //           country: "USA",
+  //         })
+  //         .then(function () {
+  //           console.log("Document successfully written!");
+  //         })
+  //     );
+  //   }
 
   return (
     <div>
@@ -15,12 +63,20 @@ function HomeSetup() {
         <Container className={classes.container}>
           <TextField
             className={classes.spacing}
-            label="Enter a Book Title"
+            label="Search"
+            placeholder="Enter a Book Title"
             margin="normal"
             variant="outlined"
+            onChange={(event) => handleClick(event.target.value)}
             fullWidth
           />
         </Container>
+        if(books)
+        {
+          <Container className={classes.container}>
+            <DisplayBooks></DisplayBooks>
+          </Container>
+        }
       </main>
     </div>
   );
