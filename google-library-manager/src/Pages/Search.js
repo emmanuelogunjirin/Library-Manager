1fd0937a0244b14d8dee02bc733e52711e8b1f43
Emmanuel Ogunjirin <eao5xc@virginia.edu>
2020-05-18 17:17:40 -0400

    Removed Unnecessary Imports

--- google-library-manager/src/Pages/Search.js
+++ google-library-manager/src/Pages/Search.js
@@ -4,24 +4,11 @@ import BookCard from "../Components/BookCard";
 import useStyles from "../Containers/User/UserStyles";
 import MainTheme from "../Containers/User/MainTheme";
 import Container from "@material-ui/core/Container";
-import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
-import Typography from "@material-ui/core/Typography";
-import ButtonBase from "@material-ui/core/ButtonBase";
-import Box from "@material-ui/core/Box";
-import Button from "@material-ui/core/Button";
 
 function Search() {
   const classes = useStyles();
   const [books, setBooks] = useState(null);
-  // const [word, setWord] = useState(null);
 
-  // const handleBookSearch = (bookRelation) => {
-  //   if (bookRelation !== "undefined") {
-  //     setWord(bookRelation);
-  //     search(word);
-  //   }
-  // };
   const search = (word) => {
     fetch("/api/books?title=" + word)
       .then((res) => res.json())
