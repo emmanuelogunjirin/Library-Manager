2aa5f390ed7bc7c042e5304b8bdd0d6be5d2d724
Emmanuel Ogunjirin <eao5xc@virginia.edu>
2020-05-18 19:36:17 -0400

    Copyright updated

--- google-library-manager/src/Components/Copyright.js
+++ google-library-manager/src/Components/Copyright.js
@@ -1,19 +1,26 @@
+/* Imports needed by the system */
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
 
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
-
+/**
+ * Function that shows the copyright style similar to the
+ * goolge way of copyrighting
+ */
 function Copyright() {
-    return (
-      <Typography variant="body2" color="textSecondary" align="center">
-        {'Copyright © '}
-        <Link color="inherit" href="https://emmanuelogunjirin1.wixsite.com/emmanuel-ogunjirin">
-          Emmanuel Ogunjirin
-        </Link>{' '}
-        {new Date().getFullYear()}
-        {'.'}
-      </Typography>
-    );
-  }
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link
+        color="inherit"
+        href="https://emmanuelogunjirin1.wixsite.com/emmanuel-ogunjirin"
+      >
+        Emmanuel Ogunjirin
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 
-export default Copyright;
+export default Copyright; // Exports the defualt function
