1df5767658164d369a7c8167515509ba89149a01
Emmanuel Ogunjirin <eao5xc@virginia.edu>
2020-05-18 18:51:54 -0400

    Updated Server.js

--- google-library-manager/server.js
+++ google-library-manager/server.js
@@ -1,39 +1,37 @@
-const express = require('express');
-const app = express();      // Actually creating a new server instance
-const fetch = require('isomorphic-fetch');
+const express = require("express"); // Sets up the express requirement
+const app = express(); // Actually creating a new server instance
+const fetch = require("isomorphic-fetch"); // Sets up the isomorphic fetch requirement
 
-app.get('/api/books', async (req, res) => {
+/* Section that gets data form the API */
+app.get("/api/books", async (req, res) => {
+  // The following is tried to by the server
   try {
-    // Grabbing the title query parameter from requests if it's provided
-    // i.e. localhost:8080/api/books?title=joy+of+baking
-    const { title } = req.query;
-    if (!title) throw new Error('You must provide a title query parameter');
+    const { title } = req.query; // Gets the title query paramaeter if provided
+    if (!title) throw new Error("You must provide a title query parameter"); // If there is no title
 
-    // Making our request to Google Books
+    /* Making our request to Google Books */
     const data = await fetch(
-      'https://www.googleapis.com/books/v1/volumes?q=' + title
-    ).then(res => res.json());
-    // Doing some data manipulation to clean up our request
-    const books = data.items.map(book => ({
+      "https://www.googleapis.com/books/v1/volumes?q=" + title
+    ).then((res) => res.json()); // Gets all possible books as a json object
+    const books = data.items.map((book) => ({
       id: book.id,
       title: book.volumeInfo.title,
-      author: book.volumeInfo.authors && book.volumeInfo.authors.join(', '),
+      author: book.volumeInfo.authors && book.volumeInfo.authors.join(", "),
       description: book.volumeInfo.description,
       rating: book.volumeInfo.averageRating,
-      image: book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.thumbnail
-    }));
+      image: book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.thumbnail,
+    })); // Maps the json object to specific values
 
-    // Returning our books to the user
-    res.json(books);
+    res.json(books); // Returns the book json object to the user
   } catch (error) {
-    // Sending an error response if anything went wrong
-    console.log(error);
-    res.status(500).json({ error });
+    /* Sending an error response if anything went wrong */
+    console.log(error); // Logs to conosle
+    res.status(500).json({ error }); // Sets the status of the page
   }
 });
 
-const port = 8080;
+const port = 8080; // Sets the port of the server
 // Listening for requests on a specific port
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+  console.log(`Server listening on port ${port}`); // Logs to console
 });
